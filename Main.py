f8309d540c1ad5ea3778a059b315cf1c3ad2c55a
Daniel <danielben-1@hotmail.com>
2021-07-18 12:05:47 +0100

    Update and rename EarlyWarning.py to Main.py

---
+++ Main.py
@@ -0,0 +1,61 @@
+
+from newsapi import NewsApiClient
+import smtplib
+from selenium import webdriver
+
+from prep import PrepModule
+
+
+browser = webdriver.Firefox()
+api= NewsApiClient(api_key = '267427d315da494f9dd6b8c907f394cf')
+source = ['BBC-News', 'Independent']
+querys =['War','Earthquake', 'EMP', 'Storm', 'virus', 'Nuclear', 'Climate', 'asteroid', 'comet', 'meteors', 'riot', 'volcano', 'inflation']
+filters = [
+    'war','EMP','EMPs', 'nuclear', 'climate change', 'storm', 'virus outbreak', 'variant',  'asteroid', 'comet', 'meteors', 'riot', 'volcano', 'inflation', 'UK', 'economy',
+    'supercell', 'Biosecurity', 'riots', 'dead', 'protesters'
+    ]
+locationlist =['Italy', 'Iceland', 'Germany', 'France ', 'Northern Atlantic Ocean', 'Spain', 'Spanien', 'Czechia']
+
+#Email stuff
+apiuser = 'sender'
+userpass = 'password'
+recever = ''
+
+if __name__ == "__main__":
+
+    RedAlert = PrepModule(api,source,querys,filters,browser, locationlist)
+
+    if RedAlert.connectiontest() == False:
+        print('No Connection to the internet')
+    else:   
+        asteroids = RedAlert.asteroid()
+        volcanos = RedAlert.volcano()
+        solar = RedAlert.Solar()
+        weather = RedAlert.Weather()
+        news = RedAlert.newssearch()
+        covid = RedAlert.covid()
+        outbreaks = RedAlert.outbreak()
+        earthquake = RedAlert.earthquake()
+
+        email_message = f''' 
+{asteroids}
+
+{volcanos}
+
+{solar}
+
+{weather}
+
+{news}
+
+{covid}
+
+{outbreaks}
+
+{earthquake}
+'''
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+                smtp_server.login(apiuser, userpass)
+                smtp_server.sendmail(str(apiuser), str(recever), email_message.encode('utf-8'))
+
