5e88a86645fc2100da62c037137da88ca3a30823
Daniel <danielben-1@hotmail.com>
2021-11-08 15:07:09 +0000

    Update Main.py
    Removed the news updates due to its lack of usefulness

--- Main.py
+++ Main.py
@@ -5,24 +5,30 @@ from selenium import webdriver
 
 from prep import PrepModule
 
+config = open('config.txt')
+config = config.readlines()
 
-browser = webdriver.Firefox()
-api= NewsApiClient(api_key = '267427d315da494f9dd6b8c907f394cf')
-source = ['BBC-News', 'Independent']
-querys =['War','Earthquake', 'EMP', 'Storm', 'virus', 'Nuclear', 'Climate', 'asteroid', 'comet', 'meteors', 'riot', 'volcano', 'inflation']
-filters = [
-    'war','EMP','EMPs', 'nuclear', 'climate change', 'storm', 'virus outbreak', 'variant',  'asteroid', 'comet', 'meteors', 'riot', 'volcano', 'inflation', 'UK', 'economy',
-    'supercell', 'Biosecurity', 'riots', 'dead', 'protesters'
-    ]
-locationlist =['Italy', 'Iceland', 'Germany', 'France ', 'Northern Atlantic Ocean', 'Spain', 'Spanien', 'Czechia']
+apikey =config[1].replace('\n', '')
+
+api= NewsApiClient(api_key = apikey)
+source = config[4].split()
+#News key words to search by
+querys = config[7].split()
+#News Filters to filter out unrelated articles
+filters = config[10].split()
+
+#Volcano area filter
+locationlist =config[13].split()
 
 #location used for flood warnings
-location = 'bs348xt'
+location = config[16]
 
-#Email stuff
-apiuser = 'sender'
-userpass = 'password'
-recever = ''
+#Email Credentials
+apiuser = config[19]
+userpass = config[22]
+recever = config[25]
+
+browser = webdriver.Firefox()
 
 if __name__ == "__main__":
 
@@ -35,22 +41,21 @@ if __name__ == "__main__":
         volcanos = RedAlert.volcano()
         solar = RedAlert.Solar()
         weather = RedAlert.Weather()
-        news = RedAlert.newssearch()
         covid = RedAlert.covid()
         outbreaks = RedAlert.outbreak()
         earthquake = RedAlert.earthquake()
         floods = RedAlert.floodwarnings()
+        #news = RedAlert.newssearch()
 
         email_message = f''' 
 {asteroids}
+See https://cneos.jpl.nasa.gov/scout/#/ for more details
 
 {volcanos}
 
 {solar}
 
 {weather}
-
-{news}
 
 {covid}
 
@@ -64,4 +69,3 @@ if __name__ == "__main__":
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
                 smtp_server.login(apiuser, userpass)
                 smtp_server.sendmail(str(apiuser), str(recever), email_message.encode('utf-8'))
-
