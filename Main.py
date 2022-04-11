4c838a63052cf8e8e75bb5616d325af8a07df443
Daniel <danielben-1@hotmail.com>
2022-04-11 11:16:54 +0100

    Major update

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 from prep import PrepModule
 
-config = open(r'RedAlert\config.txt')
+config = open('RedAlert\config.txt')
 config = config.readlines()
 
 apikey =config[1].replace('\n', '')
@@ -46,26 +46,32 @@ if __name__ == "__main__":
         earthquake = RedAlert.earthquake()
         floods = RedAlert.floodwarnings()
         #news = RedAlert.newssearch()
+        resevoir = RedAlert.resevoir_levels()
 
         email_message = f''' 
 {asteroids}
-See https://cneos.jpl.nasa.gov/scout/#/ for more details
+See https://cneos.jpl.nasa.gov/scout/#/ for more details.
 
 {volcanos}
 
 {solar}
+See https://www.spaceweatherlive.com/en/solar-activity/solar-flares.html for more information.
 
 {weather}
 
 {covid}
 
 {outbreaks}
+See https://www.who.int/emergencies/disease-outbreak-news for more information
 
 {earthquake}
 
 {floods}
+
+{resevoir}
 '''
 
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
                 smtp_server.login(apiuser, userpass)
                 smtp_server.sendmail(str(apiuser), str(recever), email_message.encode('utf-8'))
+
